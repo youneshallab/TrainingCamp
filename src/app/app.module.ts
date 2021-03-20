25d59829fbf7710af35940364f4452a1866b18ce
zrouquiAhmed <zrouqui.ahmed.Az@gmail.com>
2021-03-20 19:30:54 +0100

    wach a 3azzi

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TestcomponentComponent } from './testcomponent/testcomponent.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestcomponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
